107c4de4a039910cf68ad919cab760dadcd8bbc6
yatin5041 <86743512+yatin5041@users.noreply.github.com>
2021-07-13 00:25:09 -0500

    Update chapter_3_program.R
    Added comments with ## to comment on the functions, methods used and what they are trying to do.

--- chapter_3_program.R
+++ chapter_3_program.R
@@ -13,25 +13,28 @@
 
 print.digits <- 2  # set number of digits on print and spine chart
 
-library(support.CEs)  # package for survey construction 
+library(support.CEs)  # package for survey construction ## The package support.CEs provides basic functions that support an implementation of choice experiments
 
-# generate a balanced set of product profiles for survey
+# generate a balanced set of product profiles for survey ## Line 21-31 organizing data using Lma.design function having format Lma.design(candidate.array = NULL, attribute.names,
+## nalternatives, nblocks, row.renames = TRUE, seed = NULL)
+
 provider.survey <- Lma.design(attribute.names = 
-  list(brand = c("AT&T","T-Mobile","US Cellular","Verizon"), 
-  startup = c("$100","$200","$300","$400"), 
+  list(brand = c("AT&T","T-Mobile","US Cellular","Verizon"), ## attribute.names created using list() using variables brand,startup,monthly,service,reatil,apple,samsung,google
+  startup = c("$100","$200","$300","$400"), ## c() combines values in the list
   monthly = c("$100","$200","$300","$400"),
   service = c("4G NO","4G YES"), 
   retail = c("Retail NO","Retail YES"),
   apple = c("Apple NO","Apple YES"), 
   samsung = c("Samsung NO","Samsung YES"), 
-  google = c("Nexus NO","Nexus YES")), nalternatives = 1, nblocks=1, seed=9999)
-print(questionnaire(provider.survey))  # print survey design for review
+  google = c("Nexus NO","Nexus YES")), nalternatives = 1, nblocks=1, seed=9999) ##nalternatives is set as 1 for a binary choice experiment with an opt-out or common base option
+## nblocks is an integer value describing the number of blocks into which a choice experiment design is divided, in this case 1, Seed is random number generator
+print(questionnaire(provider.survey))  # print survey design for review ## function questionnaire () convertes CE design into CE questions used in a questionnaire survey.
 
-sink("questions_for_survey.txt")  # send survey to external text file
+sink("questions_for_survey.txt")  # send survey to external text file ## seurvey is saved in external text file
 questionnaire(provider.survey)
-sink() # send output back to the screen
+sink() # send output back to the screen ##output displayed on screen
 
-# user-defined function for plotting descriptive attribute names 
+# user-defined function for plotting descriptive attribute names ## created function effect.name.map and populated based on function effect.name
 effect.name.map <- function(effect.name) { 
   if(effect.name=="brand") return("Mobile Service Provider")
   if(effect.name=="startup") return("Start-up Cost")
@@ -43,32 +46,32 @@ effect.name.map <- function(effect.name) {
   if(effect.name=="google") return("Sells Google/Nexus Products")
   } 
 
-# read in conjoint survey profiles with respondent ranks
+# read in conjoint survey profiles with respondent ranks ## created conjoint data frame by using mobile_services_ranking.csv file
 conjoint.data.frame <- read.csv("mobile_services_ranking.csv")
 
-# set up sum contrasts for effects coding as needed for conjoint analysis
-options(contrasts=c("contr.sum","contr.poly"))
+# set up sum contrasts for effects coding as needed for conjoint analysis ##options() function examines a variety of global options which affect the way in which R computes and displays its results
+options(contrasts=c("contr.sum","contr.poly"))  ## a logical indicating whether contrasts should be computed. Here sum contarsts is used.
 
-# main effects model specification
+# main effects model specification ## ranking is column in the dataset populated by author after going through given portfolio options
 main.effects.model <- {ranking ~ brand + startup + monthly + service + 
   retail + apple + samsung + google}
 
-# fit linear regression model using main effects only (no interaction terms)
+# fit linear regression model using main effects only (no interaction terms) ## im is linear model used as lm(formula, data)
 main.effects.model.fit <- lm(main.effects.model, data=conjoint.data.frame)
-print(summary(main.effects.model.fit)) 
+print(summary(main.effects.model.fit)) ## printed summary of the model
 
 # save key list elements of the fitted model as needed for conjoint measures
 conjoint.results <- 
-  main.effects.model.fit[c("contrasts","xlevels","coefficients")]
+  main.effects.model.fit[c("contrasts","xlevels","coefficients")] ## combined values in of contrasts, xlevels and coefficients, as we are interested in those
 
-conjoint.results$attributes <- names(conjoint.results$contrasts)
+conjoint.results$attributes <- names(conjoint.results$contrasts) ## $ operator is used to extract list or subset a specific part of a data object, here contrasts
 
-# compute and store part-worths in the conjoint.results list structure
+# compute and store part-worths in the conjoint.results list structure ## this section of the code is computing and storing most important aspect part worths
 part.worths <- conjoint.results$xlevels  # list of same structure as xlevels
 end.index.for.coefficient <- 1  # intitialize skipping the intercept
-part.worth.vector <- NULL # used for accumulation of part worths
+part.worth.vector <- NULL # used for accumulation of part worths ## line 73-85 used "for loop", seq() creates sequence for elements in vector
 for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
-  nlevels <- length(unlist(conjoint.results$xlevels[index.for.attribute]))
+  nlevels <- length(unlist(conjoint.results$xlevels[index.for.attribute])) ## unlist converts list to vector
   begin.index.for.coefficient <- end.index.for.coefficient + 1
   end.index.for.coefficient <- begin.index.for.coefficient + nlevels -2
   last.part.worth <- -sum(conjoint.results$coefficients[
@@ -82,17 +85,17 @@ for(index.for.attribute in seq(along=conjoint.results$contrasts)) {
   } 
 conjoint.results$part.worths <- part.worths
 
-# compute standardized part-worths
+# compute standardized part-worths ## these values are used to plot spine chart
 standardize <- function(x) {(x - mean(x)) / sd(x)}
 conjoint.results$standardized.part.worths <- 
-  lapply(conjoint.results$part.worths,standardize)
+  lapply(conjoint.results$part.worths,standardize) ## lapply acts on a list and returns a list of the same length as input list object
  
 # compute and store part-worth ranges for each attribute 
 part.worth.ranges <- conjoint.results$contrasts
-for(index.for.attribute in seq(along=conjoint.results$contrasts)) 
+for(index.for.attribute in seq(along=conjoint.results$contrasts)) ## used for loop
   part.worth.ranges[index.for.attribute] <- 
-  dist(range(conjoint.results$part.worths[index.for.attribute]))
-conjoint.results$part.worth.ranges <- part.worth.ranges
+  dist(range(conjoint.results$part.worths[index.for.attribute])) ## Range() function returns the maximum and minimum value of the vector and column of the dataframe
+conjoint.results$part.worth.ranges <- part.worth.ranges          ## dist () computes and returns the distance matrix
 
 sum.part.worth.ranges <- sum(as.numeric(conjoint.results$part.worth.ranges))
 
@@ -104,25 +107,25 @@ for(index.for.attribute in seq(along=conjoint.results$contrasts))
   sum.part.worth.ranges) * 100
 conjoint.results$attribute.importance <- attribute.importance
  
-# data frame for ordering attribute names
+# data frame for ordering attribute names ## names() is used to get or set the name of an Object
 attribute.name <- names(conjoint.results$contrasts)
-attribute.importance <- as.numeric(attribute.importance)
+attribute.importance <- as.numeric(attribute.importance) ## as.numeric() method takes an object that needs to be coerced and returns the converted numeric value
 temp.frame <- data.frame(attribute.name,attribute.importance)
 conjoint.results$ordered.attributes <- 
-  as.character(temp.frame[sort.list(
+  as.character(temp.frame[sort.list(                      ## as.character() attempts to coerce its argument to character type
   temp.frame$attribute.importance,decreasing = TRUE),"attribute.name"])
 
-# respondent internal consistency added to list structure
+# respondent internal consistency added to list structure ## $ operator is used to extract list or subset a specific part of a data object, here r.squared
 conjoint.results$internal.consistency <- summary(main.effects.model.fit)$r.squared 
  
-# user-defined function for printing conjoint measures
+# user-defined function for printing conjoint measures ## sprintf("%1.2f") dsiplayes all digits before decimal point and 2 digits after decimal point
 if (print.digits == 2) 
   pretty.print <- function(x) {sprintf("%1.2f",round(x,digits = 2))} 
 if (print.digits == 3) 
   pretty.print <- function(x) {sprintf("%1.3f",round(x,digits = 3))} 
  
 # report conjoint measures to console 
-# use pretty.print to provide nicely formated output
+# use pretty.print to provide nicely formated output ## "\n" to print on new line and cat() combines character values and print them to the screen or directly save in a file
 for(k in seq(along=conjoint.results$ordered.attributes)) {
   cat("\n","\n")
   cat(conjoint.results$ordered.attributes[k],"Levels: ",
@@ -142,7 +145,7 @@ for(k in seq(along=conjoint.results$ordered.attributes)) {
   }
 
 # plotting of spine chart begins here
-# all graphical output is routed to external pdf file
+# all graphical output is routed to external pdf file ## conjoint.results is used to plot spine chart
 pdf(file = "fig_preference_mobile_services_results.pdf", width=8.5, height=11)
 spine.chart(conjoint.results)
 dev.off()  # close the graphics output device
